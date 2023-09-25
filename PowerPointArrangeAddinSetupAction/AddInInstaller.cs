3a4f3201b8b34a105c58549b3a481c9ceb2c908b
Aoi-hosizora <a970335605@hotmail.com>
2023-09-26 02:03:09 +0800

    feat: re-create setup project, and finish installing procedure with updating registry manually

--- PowerPointArrangeAddinSetupAction/AddInInstaller.cs
+++ PowerPointArrangeAddinSetupAction/AddInInstaller.cs
@@ -2,124 +2,169 @@
 using System.Collections;
 using System.ComponentModel;
 using System.Configuration.Install;
-using System.Diagnostics;
 using System.IO;
-using System.Reflection;
-using System.Threading;
-using System.Windows.Forms;
+using System.Xml;
+using Microsoft.Win32;
+
+// ReSharper disable CheckNamespace
+// ReSharper disable UnusedMember.Global
+
+#nullable enable
 
 [RunInstaller(true)]
 public class AddInInstaller : Installer {
 
+    #region Overrided Methods
+
+    public override void Install(IDictionary savedState) {
+        base.Install(savedState);
+
+        AddInRegistryEntry entry;
+        try {
+            entry = GetAddInRegistryEntry();
+        } catch (Exception) {
+            throw new InstallException("Failed to read manifest file.");
+        }
+
+        try {
+            UpdateRegistry(entry);
+        } catch (Exception) {
+            throw new InstallException("Failed to update registry");
+        }
+    }
+
+    public override void Uninstall(IDictionary? savedState) {
+        base.Uninstall(savedState);
+
+        AddInRegistryEntry entry;
+        try {
+            entry = GetAddInRegistryEntry();
+        } catch (Exception) {
+            throw new InstallException("Failed to read manifest file.");
+        }
+
+        try {
+            DeleteRegistry(entry);
+        } catch (Exception) {
+            throw new InstallException("Failed to update registry");
+        }
+    }
+
+    #endregion
+
+    #region Helper Methods
+
     private string GetTargetDir() {
-        //return @"C:\Program Files\AoiHosizora\PowerPointArrangeAddin";
         var targetDir = Context.Parameters["dir"].TrimEnd('/', '\\');
         return targetDir;
     }
 
-    private string GetVstoInstallerPath() {
-        //return @"C:\Program Files\AoiHosizora\PowerPointArrangeAddin\VSTOInstaller.exe";
-        //return @"C:\Program Files\Common Files\Microsoft Shared\VSTO\10.0\VSTOInstaller.exe";
-        return System.IO.Path.Combine(GetTargetDir(), "VSTOInstaller.exe");
-    }
-
     private string GetVstoFilePath() {
-        //return @"E:\Projects\PowerPointArrangeAddin\PowerPointArrangeAddin\bin\x64\Release\PowerPointArrangeAddin.vsto";
-        //return "C:\\Program Files\\AoiHosizora\\PowerPointArrangeAddin\\PowerPointArrangeAddin.vsto";
-        //return "file:///C:/Program%20Files/AoiHosizora/PowerPointArrangeAddin/PowerPointArrangeAddin.vsto";
-        return System.IO.Path.Combine(GetTargetDir(), "PowerPointArrangeAddin.vsto");
+        return Path.Combine(GetTargetDir(), "PowerPointArrangeAddin.vsto");
     }
 
-    public override void Install(IDictionary savedState) {
-        base.Install(savedState);
-        //Directory.SetCurrentDirectory(GetTargetDir());
-        //MessageBox.Show(Path.GetDirectoryName
-        //    (Assembly.GetExecutingAssembly().Location));
-        //MessageBox.Show(GetVstoFilePath() + ": " + (File.Exists(GetVstoFilePath()) ? "true" : "false") + "; " + GetVstoInstallerPath());
-        //var psi = new ProcessStartInfo {
-        //    FileName = GetVstoInstallerPath(),
-        //    Arguments = $"/Install \"{GetVstoFilePath()}\"",
-        //    WorkingDirectory = GetTargetDir()
-        //}
-        var psi = new ProcessStartInfo {
-            FileName = @"C:\Program Files\Common Files\Microsoft Shared\VSTO\10.0\VSTOInstaller.exe",
-            Arguments = @"/Install ""C:\Program Files\AoiHosizora\PowerPointArrangeAddin\PowerPointArrangeAddin.vsto""",
-            WorkingDirectory = @"C:\Program Files\AoiHosizora\PowerPointArrangeAddin",
-            // EnvironmentVariables = { { "APPDATA", "" } },
-            // Environment = { { "PATH", $@"C:\Program Files\AoiHosizora\PowerPointArrangeAddin; {Environment.GetEnvironmentVariable("PATH")}" } },
-            UseShellExecute = false,
-            Verb = "runas"
+    private string GetManifestFilePath() {
+        return Path.Combine(GetTargetDir(), "PowerPointArrangeAddin.dll.manifest");
+    }
+
+    private struct AddInRegistryEntry {
+        public string DllName { get; set; }
+        public string FriendlyName { get; set; }
+        public string Description { get; set; }
+        public int LoadBehavior { get; set; }
+        public string ManifestPath { get; set; }
+    }
+
+    private AddInRegistryEntry GetAddInRegistryEntry() {
+        var manifestPath = GetManifestFilePath();
+        var doc = new XmlDocument();
+        doc.Load(manifestPath); // allow throwing here
+
+        var entry = new AddInRegistryEntry {
+            DllName = "",
+            FriendlyName = "",
+            Description = "",
+            LoadBehavior = 0,
+            ManifestPath = $"file://{GetVstoFilePath().Replace("\\", "/")}|vstolocal"
         };
-        var p = new Process { StartInfo = psi };
-        var ok = p.Start(); // TODO
-        if (!ok) {
-            throw new InstallException("Cannot execute VSTO installer.");
+
+        var asmv1 = "urn:schemas-microsoft-com:asm.v1";
+        var vstov4 = "urn:schemas-microsoft-com:vsto.v4";
+
+        var assemblyIdentityElements = doc.GetElementsByTagName("assemblyIdentity", asmv1);
+        if (assemblyIdentityElements.Count > 0) {
+            entry.DllName = assemblyIdentityElements[0].Attributes?["name"]?.Value ?? "";
+            if (entry.DllName.EndsWith(".dll")) {
+                entry.DllName = entry.DllName.Substring(0, entry.DllName.Length - 4);
+            }
         }
-        p.WaitForExit();
-        if (p.ExitCode != 0) {
-            throw new InstallException("Failed to install VSTO.");
+
+        var friendlyNameElements = doc.GetElementsByTagName("friendlyName", vstov4);
+        if (friendlyNameElements.Count > 0) {
+            entry.FriendlyName = friendlyNameElements[0].InnerText;
+        }
+
+        var descriptionElements = doc.GetElementsByTagName("description", vstov4);
+        if (descriptionElements.Count > 0) {
+            entry.Description = descriptionElements[0].InnerText;
+        }
+
+        var appAddInElements = doc.GetElementsByTagName("appAddIn", vstov4);
+        if (appAddInElements.Count > 0) {
+            if (int.TryParse(appAddInElements[0].Attributes?["loadBehavior"]?.Value ?? "0", out var value)) {
+                entry.LoadBehavior = value;
+            }
+        }
+
+        return entry;
+    }
+
+    private RegistryKey RegistryKey => Registry.LocalMachine;
+
+    private void UpdateRegistry(AddInRegistryEntry entry) {
+        var addinsKeyPath = @"SOFTWARE\Microsoft\Office\PowerPoint\Addins";
+        var addinsKey = RegistryKey.OpenSubKey(addinsKeyPath, RegistryKeyPermissionCheck.ReadWriteSubTree);
+        addinsKey ??= RegistryKey.CreateSubKey(addinsKeyPath, RegistryKeyPermissionCheck.ReadWriteSubTree);
+        if (addinsKey == null) {
+            throw new NullReferenceException(nameof(addinsKey));
+        }
+
+        var addinKeyName = $"AoiHosizora.{entry.DllName}";
+        var addinKey = addinsKey.OpenSubKey(addinKeyName);
+        if (addinKey != null) {
+            addinsKey.DeleteSubKeyTree(addinKeyName);
+        }
+        addinKey = addinsKey.CreateSubKey(addinKeyName);
+        if (addinKey == null) {
+            throw new NullReferenceException(nameof(addinKey));
+        }
+
+        addinKey.SetValue("Description", entry.Description, RegistryValueKind.String);
+        addinKey.SetValue("FriendlyName", entry.FriendlyName, RegistryValueKind.String);
+        addinKey.SetValue("LoadBehavior", entry.LoadBehavior, RegistryValueKind.DWord);
+        addinKey.SetValue("Manifest", entry.ManifestPath, RegistryValueKind.String);
+
+        addinKey.Close();
+        addinsKey.Close();
+    }
+
+    private void DeleteRegistry(AddInRegistryEntry entry) {
+        var addinsKeyPath = @"SOFTWARE\Microsoft\Office\PowerPoint\Addins";
+        var addinsKey = RegistryKey.OpenSubKey(addinsKeyPath, RegistryKeyPermissionCheck.ReadWriteSubTree);
+        if (addinsKey == null) {
+            return;
+        }
+
+        var addinKeyName = $"AoiHosizora.{entry.DllName}";
+        try {
+            addinsKey.DeleteSubKeyTree(addinKeyName);
+        } catch (Exception) {
+            // ignored
+        } finally {
+            addinsKey.Close();
         }
     }
 
-    public override void Uninstall(IDictionary savedState) {
-        base.Uninstall(savedState);
-        // Thread.Sleep(2000);
-        // var psi = new ProcessStartInfo {
-        //     FileName = GetVstoInstallerPath(),
-        //     Arguments = $"/Uninstall {GetVstoFilePath()}",
-        //     WorkingDirectory = GetTargetDir()
-        // };
-        // var p = Process.Start(psi);
-        // if (p == null) {
-        //     throw new InstallException("Cannot execute VSTO uninstaller.");
-        // }
-        // p?.WaitForExit();
-        // if (p.ExitCode != 0) {
-        //     throw new InstallException("Failed to uninstall VSTO.");
-        // }
-    }
-
-    //public void Install() {
-    //    System.Windows.Forms.MessageBox.Show(GetVstoFilePath() + ": " + (File.Exists(GetVstoFilePath()) ? "true" : "false") + "; " + GetVstoInstallerPath());
-    //    var psi = new ProcessStartInfo {
-    //        FileName = GetVstoInstallerPath(),
-    //        Arguments = $"/Install \"{GetVstoFilePath()}\"",
-    //        WorkingDirectory = GetTargetDir()
-    //    };
-    //    var p = Process.Start(psi); // TODO
-    //    if (p == null) {
-    //        throw new InstallException("Cannot execute VSTO installer.");
-    //    }
-    //    p.WaitForExit();
-    //    if (p.ExitCode != 0) {
-    //        throw new InstallException("Failed to install VSTO.");
-    //    }
-    //}
-
-    //public void Uninstall() {
-    //    System.Windows.Forms.MessageBox.Show(GetVstoFilePath() + ": " + (File.Exists(GetVstoFilePath()) ? "true" : "false") + "; " + GetVstoInstallerPath());
-    //    var psi = new ProcessStartInfo {
-    //        FileName = GetVstoInstallerPath(),
-    //        Arguments = $"/Uninstall \"{GetVstoFilePath()}\"",
-    //        WorkingDirectory = GetTargetDir()
-    //    };
-    //    var p = Process.Start(psi); // TODO
-    //    if (p == null) {
-    //        throw new InstallException("Cannot execute VSTO installer.");
-    //    }
-    //    p.WaitForExit();
-    //    if (p.ExitCode != 0) {
-    //        throw new InstallException("Failed to install VSTO.");
-    //    }
-    //}
-
-    //public static void Main(string[] args) {
-    //    var installer = new AddInInstaller();
-    //    if (args.Length >= 2 && (args[1].ToLower() == "/u" || args[1].ToLower() == "/uninstaller")) {
-    //        installer.Uninstall();
-    //    } else {
-    //        installer.Install();
-    //    }
-    //}
+    #endregion
 
 }
--- PowerPointArrangeAddinSetupAction/Properties/AssemblyInfo.cs
+++ PowerPointArrangeAddinSetupAction/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e173069d-059c-48c3-bf62-da731f0fd33f")]
+[assembly: Guid("cd517063-5c97-49db-93c2-3e31dcfe0203")]
 
 // Version information for an assembly consists of the following four values:
 //
